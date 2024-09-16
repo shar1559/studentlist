40f5c2e04bc72e05f70709622617d21a4a383431
ayush <ayush@10.48.19.194>
2024-09-16 09:57:51 -0400

    initial commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fagun at 12/05/2024
+ */
+public class Student {
+    private String name;
+    private String studentID;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+     public String getaddress() {
+        return name;
+    }
+
+    public void setaddress(String address) {
+        this.address = address;
+    }
+     public String getstudentID() {
+        return name;
+    }
+
+    public void setstudentId(String name) {
+        this.name = name;
+    }
+   
+}
