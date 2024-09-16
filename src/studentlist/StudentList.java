f18421c2e1469ceb5b66f29b1fc24bcd62ebc780
ayush <ayush@10.48.19.194>
2024-09-16 10:13:56 -0400

    UPDATE

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -15,7 +15,7 @@ public class StudentList {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Student[] studentList = new Student[3];//change size before running
+        Student[] studentList = new Student[5];//change size before running
         Student s1 = new Student();
         s1.setName("Fagun Vankawala");
         Student s2 = new Student();
